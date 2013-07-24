8f2f410fb5eee0bdfd603ca7bd67e1375d535141
Bucky Townsend <desnudopenguino@gmail.com>
2013-07-24 12:45:44 -0400

    fixed image for attraction_tour show view

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -13,28 +13,19 @@ class MediaController < ApplicationController
 
   # GET /media/1
   # GET /media/1.json
-  def show
-    @medium = Medium.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @medium }
-    end
-  end
-
-  # GET /media/new
-  # GET /media/new.json
-  def new
-    @medium = Medium.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @medium }
-    end
-  end
+#  
+#  def new
+#    @medium = Medium.new
+#
+#    respond_to do |format|
+#      format.html # new.html.erb
+#      format.json { render json: @medium }
+#    end
+#  end
 
   # GET /media/1/edit
   def edit
+    @multimedia = find_media 
     @medium = Medium.find(params[:id])
   end
 
@@ -62,24 +53,20 @@ class MediaController < ApplicationController
   # PUT /media/1.json
   def update
     @medium = Medium.find(params[:id])
-
+    @medium.update_attributes(params[:medium])
+    @parent = @medium.multimedia
     respond_to do |format|
-      if @medium.update_attributes(params[:medium])
-        format.html { redirect_to @medium, notice: 'Medium was successfully updated.' }
+        format.html { render edit_medium(@medium) }
         format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @medium.errors, status: :unprocessable_entity }
-      end
     end
   end
 
   # DELETE /media/1
   # DELETE /media/1.json
   def destroy
+    @multimedia = find_media
     @medium = Medium.find(params[:id])
     @medium.destroy
-    @multimedia = find_media
 
     respond_to do |format|
       format.html { redirect_to @multimedia }
