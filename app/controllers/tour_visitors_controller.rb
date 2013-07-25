f9c1f733d0a3caf4116a47479dc643c38e551c15
Bucky Townsend <desnudopenguino@gmail.com>
2013-07-25 12:19:07 -0400

    added json return for updating tour_visitor

--- app/controllers/tour_visitors_controller.rb
+++ app/controllers/tour_visitors_controller.rb
@@ -71,7 +71,7 @@ class TourVisitorsController < ApplicationController
     respond_to do |format|
       if @tour_visitor.update_attributes(params[:tour_visitor])
         format.html { redirect_to @tour_visitor, notice: 'Tour visitor was successfully updated.' }
-        format.json { head :no_content }
+        format.json { render @tour_visitor }
       else
         format.html { render action: "edit" }
         format.json { render json: @tour_visitor.errors, status: :unprocessable_entity }
