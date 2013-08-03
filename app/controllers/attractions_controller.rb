70c83e884a0e0e1412e4f103e3b6070e2f480b8e
Bucky Townsend <desnudopenguino@gmail.com>
2013-08-03 01:53:04 -0400

    attraction edit redirect points back to attraction

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -68,13 +68,11 @@ class AttractionsController < ApplicationController
   # PUT /attractions/1
   # PUT /attractions/1.json
   def update
-    @venue = Venue.find(params[:venue_id])
     @attraction = Attraction.find(params[:id])
-    
 
     respond_to do |format|
       if @attraction.update_attributes(params[:attraction])
-        format.html { redirect_to attraction_path(@attraction), notice: 'Attraction was successfully updated.' }
+        format.html { redirect_to @attraction, notice: 'Attraction was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
